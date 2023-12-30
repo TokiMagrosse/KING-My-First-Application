e771ba3e37c66d561376d4a51495ac7958ecaf9b
TokiMagrosse <144586591+TokiMagrosse@users.noreply.github.com>
2023-12-30 21:37:41 +0400

    First update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button guest_button, signin_button, signup_button;
+    TextView king;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        guest_button = findViewById(R.id.guest_button);
+        signin_button = findViewById(R.id.sign_in_button);
+        signup_button = findViewById(R.id.sign_up_button);
+        king = findViewById(R.id.title_king);
+
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
