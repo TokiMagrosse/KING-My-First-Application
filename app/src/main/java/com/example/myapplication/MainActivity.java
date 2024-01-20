4c68f0db8a070994c8252218cc880a5faba525b5
TokiMagrosse <144586591+TokiMagrosse@users.noreply.github.com>
2024-01-21 00:17:53 +0400

    Updated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,17 +7,40 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView register_text_reference, forgot_password;
+    Button login_button;
+    CheckBox remember_me;
+    EditText email_address, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register_text_reference = findViewById(R.id.register_text_reference);
+        forgot_password = findViewById(R.id.forgot_password);
+        login_button = findViewById(R.id.login_button);
+        remember_me = findViewById(R.id.remember_me);
+        email_address = findViewById(R.id.email_address);
+        password = findViewById(R.id.password);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        register_text_reference.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
+            /*private void startActivity(Intent intent) {
+
+            }*/
+        });
     }
 
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,16 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button register_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        register_button = findViewById(R.id.register_button);
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                // Toast.makeText(this, "Verify your account by checking your email");
+            }
+        });
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
