0b1cb2eacd31b0f574d6206f7f6313c786766d0d
TokiMagrosse <144586591+TokiMagrosse@users.noreply.github.com>
2024-03-13 20:06:44 +0400

    Poisonous King: Update 37

---
+++ app/src/main/java/com/example/poisonousking/GameFieldActivity.java
@@ -0,0 +1,34 @@
+package com.example.poisonousking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameFieldActivity extends AppCompatActivity {
+
+    Button back_to_profile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_game_field);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        back_to_profile = findViewById(R.id.back_to_profile);
+        back_to_profile.setOnClickListener(v -> {
+            Intent intent = new Intent(GameFieldActivity.this, UserProfileAttributesActivity.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/com/example/poisonousking/ProfileActivity.java
+++ app/src/main/java/com/example/poisonousking/ProfileActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,7 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button log_out_button;
+    Button log_out_button, play_button;
     ImageView your_profile_picture;
     TextView your_username;
     FirebaseUser user;
@@ -29,10 +31,17 @@ public class ProfileActivity extends AppCompatActivity {
 
         FirebaseFirestore f_store = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
-        log_out_button = findViewById(R.id.log_out_button);
+        // log_out_button = findViewById(R.id.log_out_button);
         your_profile_picture = findViewById(R.id.your_profile_picture);
         your_username = findViewById(R.id.your_username);
+        play_button = findViewById(R.id.play_button);
         user = auth.getCurrentUser();
+
+        play_button.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, GameFieldActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Your game will start soon. Good luck!", Toast.LENGTH_LONG).show();
+        });
 
         your_profile_picture.setOnClickListener(v -> {
             Intent intent = new Intent(ProfileActivity.this, UserProfileAttributesActivity.class);
@@ -59,12 +68,12 @@ public class ProfileActivity extends AppCompatActivity {
             });
         }
 
-        log_out_button.setOnClickListener(v -> {
+        /*log_out_button.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
-        });
+        });*/
 
     }
 }
