2b27db56c5c9926d2e08e06d0b093532164bf7a0
TokiMagrosse <144586591+TokiMagrosse@users.noreply.github.com>
2024-04-05 22:49:58 +0400

    Also created 4 lists for 4 suits of 32-deck card + working on logic NO BOYS(Jacks)

--- app/src/main/java/com/example/poisonousking/GameFieldActivity.java
+++ app/src/main/java/com/example/poisonousking/GameFieldActivity.java
@@ -14,8 +14,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.poisonousking.R;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,14 +26,14 @@ public class GameFieldActivity extends AppCompatActivity {
     Button back_to_profile;
     private ImageView[] image_views;
     private final int[] card_images = {
-            R.drawable.ace_of_clubs, R.drawable.ace_of_diamonds, R.drawable.ace_of_hearts, R.drawable.ace_of_spades,
-            R.drawable.king_of_clubs, R.drawable.king_of_diamonds, R.drawable.ace_of_hearts, R.drawable.king_of_spades,
-            R.drawable.queen_of_clubs, R.drawable.queen_of_diamonds, R.drawable.queen_of_hearts, R.drawable.queen_of_spades,
-            R.drawable.jack_of_clubs, R.drawable.jack_of_diamonds, R.drawable.jack_of_hearts, R.drawable.jack_of_spades,
-            R.drawable.ten_of_clubs, R.drawable.ten_of_diamonds, R.drawable.ten_of_hearts, R.drawable.ten_of_spades,
-            R.drawable.nine_of_clubs, R.drawable.nine_of_diamonds, R.drawable.nine_of_hearts, R.drawable.nine_of_spades,
+            R.drawable.seven_of_clubs, R.drawable.seven_of_diamonds, R.drawable.seven_of_hearts, R.drawable.seven_of_spades,
             R.drawable.eight_of_clubs, R.drawable.eight_of_diamonds, R.drawable.eight_of_hearts, R.drawable.eight_of_spades,
-            R.drawable.seven_of_clubs, R.drawable.seven_of_diamonds, R.drawable.seven_of_hearts, R.drawable.seven_of_spades
+            R.drawable.nine_of_clubs, R.drawable.nine_of_diamonds, R.drawable.nine_of_hearts, R.drawable.nine_of_spades,
+            R.drawable.ten_of_clubs, R.drawable.ten_of_diamonds, R.drawable.ten_of_hearts, R.drawable.ten_of_spades,
+            R.drawable.jack_of_clubs, R.drawable.jack_of_diamonds, R.drawable.jack_of_hearts, R.drawable.jack_of_spades,
+            R.drawable.queen_of_clubs, R.drawable.queen_of_diamonds, R.drawable.queen_of_hearts, R.drawable.queen_of_spades,
+            R.drawable.king_of_clubs, R.drawable.king_of_diamonds, R.drawable.ace_of_hearts, R.drawable.king_of_spades,
+            R.drawable.ace_of_clubs, R.drawable.ace_of_diamonds, R.drawable.ace_of_hearts, R.drawable.ace_of_spades
     };
 
     @Override
@@ -49,6 +47,8 @@ public class GameFieldActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Card doors that will block players card(s) changing his color
+        // green - means you can throw it, red - means you can't throw it
         card_door_1 = findViewById(R.id.card_door_1);
         card_door_2 = findViewById(R.id.card_door_2);
         card_door_3 = findViewById(R.id.card_door_3);
@@ -58,6 +58,7 @@ public class GameFieldActivity extends AppCompatActivity {
         card_door_7 = findViewById(R.id.card_door_7);
         card_door_8 = findViewById(R.id.card_door_8);
 
+        // Four cells for the cards in the center
         center_card_1 = findViewById(R.id.center_card_1);
         center_card_2 = findViewById(R.id.center_card_2);
         center_card_3 = findViewById(R.id.center_card_3);
@@ -73,8 +74,30 @@ public class GameFieldActivity extends AppCompatActivity {
         image_views[6] = findViewById(R.id.my_card_7);
         image_views[7] = findViewById(R.id.my_card_8);
 
-        distributeRandomCards();
+        // distributeRandomCards();
+        List<Integer> deck = generateDeckOfCards();
+        Collections.shuffle(deck);
 
+        // Divide the deck into 4 lists, each representing the cards for one player
+        List<Integer> users_cards = deck.subList(0, 8);
+        List<Integer> first_bot_cards = deck.subList(8, 16);
+        List<Integer> second_bot_cards = deck.subList(16, 24);
+        List<Integer> third_bot_cards = deck.subList(24, 32);
+
+        // Assign the cards to the ImageViews for the first player
+        for (int i = 0; i < 8; i++) {
+            image_views[i].setImageResource(users_cards.get(i));
+        }
+
+        // Lists of each suit sorted by increasing of card values
+        List<Integer> CLUBS = allIDesOfClubsSorted();
+        List<Integer> DIAMONDS = allIDesOfDiamondsSorted();
+        List<Integer> HEARTS = allIDesOfHeartsSorted();
+        List<Integer> SPADES = allIDesOfSpadesSorted();
+
+
+
+        // Button that brings user into the main page of the game
         back_to_profile = findViewById(R.id.back_to_profile);
         back_to_profile.setOnClickListener(v -> {
             Toast.makeText(this, "You left the game", Toast.LENGTH_SHORT).show();
@@ -82,6 +105,7 @@ public class GameFieldActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        // Logic of throwing the card from bottom to center
         image_views[0].setOnClickListener(v -> {
             center_card_1.setImageDrawable(image_views[0].getDrawable());
             center_card_1.setVisibility(View.VISIBLE);
@@ -131,24 +155,6 @@ public class GameFieldActivity extends AppCompatActivity {
             card_door_8.setVisibility(View.GONE);
         });
 
-
-    }
-
-    private void distributeRandomCards() {
-        List<Integer> deck = generateDeckOfCards();
-        Collections.shuffle(deck);
-
-        // Divide the deck into 4 lists, each representing the cards for one player
-        List<Integer> users_cards = deck.subList(0, 8);
-        List<Integer> first_bot_cards = deck.subList(8, 16);
-        List<Integer> second_bot_cards = deck.subList(16, 24);
-        List<Integer> third_bot_cards = deck.subList(24, 32);
-
-        // Assign the cards to the ImageViews for the first player
-        for (int i = 0; i < 8; i++) {
-            image_views[i].setImageResource(users_cards.get(i));
-        }
-
     }
 
     private List<Integer> generateDeckOfCards() {
@@ -158,6 +164,38 @@ public class GameFieldActivity extends AppCompatActivity {
             deck.add(image_id);
         }
         return deck;
+    }
+
+    private List<Integer> allIDesOfClubsSorted() {
+        List<Integer> clubs = new ArrayList<>();
+        for (int i = 0; i < card_images.length; i += 4) {
+            clubs.add(card_images[i]);
+        }
+        return clubs;
+    }
+
+    private List<Integer> allIDesOfDiamondsSorted() {
+        List<Integer> diamonds = new ArrayList<>();
+        for (int i = 1; i < card_images.length; i += 4) {
+            diamonds.add(card_images[i]);
+        }
+        return diamonds;
+    }
+
+    private List<Integer> allIDesOfHeartsSorted() {
+        List<Integer> hearts = new ArrayList<>();
+        for (int i = 2; i < card_images.length; i += 4) {
+            hearts.add(card_images[i]);
+        }
+        return hearts;
+    }
+
+    private List<Integer> allIDesOfSpadesSorted() {
+        List<Integer> spades = new ArrayList<>();
+        for (int i = 3; i < card_images.length; i += 4) {
+            spades.add(card_images[i]);
+        }
+        return spades;
     }
 
 }
