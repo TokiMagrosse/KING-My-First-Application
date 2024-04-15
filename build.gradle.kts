e49da126415075290887a7f17cc0e693b00ff0f5
TokiMagrosse <144586591+TokiMagrosse@users.noreply.github.com>
2024-04-16 00:06:35 +0400

    COOOOOL

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 plugins {
     // To benefit from the latest Performance Monitoring plugin features,
     // update your Android Gradle plugin dependency to at least v3.4.0
-    id("com.android.application") version "8.3.2" apply false
+    id("com.android.application") version "8.4.0-rc02" apply false
 
     // Make sure that you have the Google services Gradle plugin dependency
     id("com.google.gms.google-services") version "4.4.1" apply false
